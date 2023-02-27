ac22ada736715c425c343051749ffd605f86719e
Dmitry-757 <glv11.glv11.glv11@gmail.com>
2023-02-27 16:56:05 +0300

    ...

--- src/main/java/org/dng/beer_counters/model/ProductionInfo.java
+++ src/main/java/org/dng/beer_counters/model/ProductionInfo.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-@Entity
+@Entity(name = "production_info")
 public class ProductionInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
